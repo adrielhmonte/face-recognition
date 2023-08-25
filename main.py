49f39235142892ff1d20308231b4272c72a0f4db
adrielhmonte <adrielhmc@outlook.com>
2023-08-25 19:23:57 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import cv2 as cv
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_face_mesh = mp.solutions.face_mesh
+
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+cam = cv.VideoCapture(0)
+
+with mp_face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5,
+                           min_tracking_confidence=0.5) as face_mesh:
+
+    while cam.isOpened():
+        success, frame = cam.read()
+        if not success:
+            print("Ignoring this frame")
+            continue
+
+        frame.flags.writeable = False
+        results = face_mesh.process(frame)
+
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                mp_drawing.draw_landmarks(
+                    image=frame,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_TESSELATION,
+                    landmark_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style()
+            )
+                mp_drawing.draw_landmarks(
+                    image=frame,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_IRISES,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_iris_connections_style()
+                )
+
+        cv.imshow('mediapipe face mesh', cv.flip(frame, 1))
+        if cv.waitKey(1) == ord('q'):
+            break
+
+cam.release()
+cv.destroyAllWindows()
